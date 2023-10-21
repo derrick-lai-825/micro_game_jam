496d684322acb2d923baa7f00eb8923ef44a7162
Codebolt <codebolt7@gmail.com>
2023-10-21 15:22:59 -0700

    Updated GameManager

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,17 +1,21 @@
 extends Node
 	
 @onready var music = $Music
+@onready var timer = $Timer
 
 @export var timer_duration: float = 10
 @export var timer_end: float = 0.1
 @export var conveyer_speed: float = 1
 @export var music_bpm: int = 120
 
+var timer_interval: float = 60.0 / music_bpm
+var num_of_bots: int = 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	MicrogameJamController.SetMaxTimer(timer_duration)
 	music.play()
+	timer.start(timer_interval)
 	pass # Replace with function body.
 
 
@@ -24,8 +28,9 @@ func _process(delta):
 		print("WIN!!")
 	pass
 
-func GetBPM() -> int:
-	return music_bpm;
-
 func GetConveyerSpeed() -> float:
 	return conveyer_speed;
+
+func _on_timer_timeout():
+	# Spawn Bot
+	pass # Replace with function body.
